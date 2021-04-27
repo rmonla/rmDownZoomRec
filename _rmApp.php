caddfebc2a1621594cedd72b304e57ff89a3b958
Ricardo MONLA <rmonla@gmail.com>
2021-04-26 21:24:37 -0300

    1.21.7 => LISTA - Agrega una lista que se incrementa con comandos zoomdl.

---
+++ _rmApp.php
@@ -0,0 +1,32 @@
+<?php 
+    // LOCAL http://localhost/www/dticUTNLR/aulavirtual/_v2/_src/rmDownZoomRec
+
+    define('APPNOM', 'rmDownZoomRec');
+    define('APPDET', '
+
+      ## Formulario que retorna comando zoomdl. ## 
+      
+      El objetivo principal es el de generar, desde una url de zoom de una grabación, un commando de descarga para uno o varioas archivos y descargarlos en un servidor linux con un formato de fecha y nombre personalizado. Luego subir los archivos descargados a un canal de YouTube.
+
+      ');
+
+    define('APPAUT', 'Ricardo MONLA <rmonla@gmail.com>');
+    define('APPDEV', 'https://github.com/rmonla/rmDownZoomRec');
+
+    define('APPINSTALL', '
+      _src/composer require somesh/php-query
+      _src/composer require formr/formr
+
+      ');
+
+    $appVers = [
+      '1.21.7' => 'LISTA - Agrega una lista que se incrementa con comandos zoomdl.',
+      '1.21.6' => 'rmApp - Agrrega Detalles del código',
+      '1.21.0' => 'Inicia codificación'
+    ];
+    
+    $vers = array_keys($appVers);
+    
+    define('APPVER', $vers[0]);
+
+ ?>
--- index.php
+++ index.php
@@ -1,25 +1,6 @@
 <?php  
-
-	define('APPVER', '1.21.2');
-	define('APPNOM', 'rmDZoomRec');
-
-	define('APPDET', '
-		
-		## Generador de comando de descarga zoomdl. ## 
-		El objetivo principal es el de generar un comando de descarga que se utiliza en un servidor linux que como parámetro se necesita una URL de una grabación Zoom que no esté protegida con contraseña.
-
-		');
-
-	define('APPAUT', 'Ricardo MONLA <rmonla@gmail.com>');
-	define('APPLNKDEV', 'https://github.com/rmonla/rmDZoomRec');
-	
-	define('APPREQ', '
-		_src/composer require somesh/php-query
-		_src/composer require formr/formr
-
-		');
-
-  // rrr
+  
+  include_once '_rmApp.php';
 
 ?>
 
@@ -48,15 +29,21 @@
     $dirLog = '/tmp/';
     $strSEP = ' | ';
 
+    $aCMDs  = [];
+
+  // Instacio la class Formr
     $form = new Formr\Formr('bootstrap');
-    $form->create_form('urlORI|url');
+    $form->open();
+    $form->text('urlOri','URL');
+
+    
+    // $form->text('urlori','URL ORI');
+    // $form->submit_button('Submit Form');
 
   // check if the form has been submitted
-  if( $form->submitted() ){
-     // $urlOri = 'https://utn.zoom.us/rec/share/0EXhZfZpcsLf0nEoWrHyU9XdlrYP2N4BnFJJelMr5n9F83OrhsniHrRNUsoh-GKd.mOcTfpb755WvRZT1';
-
+  if( $form->submitted() ) {
     // Inicializo variables.
-      $urlOri  = $form->post('urlori');
+      $urlOri  = $form->post('urlOri');
 
     // Obtengo el HTML de la URL y genero el objeto DOM.
       $srtHTML = file_get_contents($urlOri);
@@ -73,58 +60,52 @@
       // <input type="hidden" id="r_meeting_start_time" value="Feb 18, 2021 8:28 PM Buenos Aires, Georgetown" />
       //--> Feb 18, 2021 8:28 PM Buenos Aires, Georgetown
       
-      $strPeso = trim($dom['.ipad-hide']->html());
-      //--> Download (798 MB)
+      $strPeso = trim($dom['.ipad-hide']->html()); //--> Download (798 MB)
 
     // Proceso el título.
-
       if (!$strTit) exit ('Error: No se pudo obtener los datos de la URL');
 
       $strTit = utf8_decode($strTit);
 
     // Proceso la fecha.
-
       $strD = explode(' ', $strData);
 
       $strF = '';
-      for ($i=0; $i < 5; $i++) $strF .= $strD[$i].' ';
-      //--> Feb 18, 2021 8:28 PM
+      for ($i=0; $i < 5; $i++) $strF .= $strD[$i].' ';     //--> Feb 18, 2021 8:28 PM
 
-      $codF = date_format(date_create($strF), 'd.M.H:i.');
-      //--> 18Feb21_20228
+      $codF = date_format(date_create($strF), 'd.M.H:i.'); //--> 18.Feb.14:00
 
     // Proceso el nombre del archivo.
-      
       $fNOM = trim($codF).trim($strTit);
       
-      //$fNOM = utf8_encode($fNOM);
-      
-      //$arrTit = explode($strSEP, $strTit);
-      //--> ['PI21IntCA', 'Pre-Ingreso2021 Intensivo [Comisión A]', 'UTNLaRioja']
-
-      //$arrTit[0] = trim($arrTit[0]).trim($codF);
-      //--> PI21IntCA_18Feb21_20228
-      
-      // $fNOM = implode(' | ', $arrTit);
-      
-      // $fNOM = str_replace(" ", "_", trim($fNOM));
-
-
     // Proceso el commado a ejecutar.
-      
       $fDST = $dirDst.$fNOM;
       // $fLOG = $dirLog.$fNOM.'.log';
 
       $fLOG = ($logEST) ? ' > '.$dirLog.$fNOM.'.log': '' ;
       
-      echo "<br><p>zoomdl -u $urlOri -f '$fDST'$fLOG</p>";
+      $cmdActual = "zoomdl -u $urlOri -f '$fDST'$fLOG";
+  
+    // Armo la lista de Comandos.
 
-      echo "<p>$strTit<br>$strData<br>$strPeso</p>";
-      
-  } else {
+      if ( $cmdHist = $form->post('cmdHist') ) $aCMDs = explode("©", $cmdHist);
+
+      $aCMDs[] = $cmdActual;
+
+      $form->hidden('cmdHist',implode("©", $aCMDs));
 
   }
+  $form->submit_button();
+  $form->close();
   
+  $lst = implode("\n", $aCMDs)."\n\n"; 
+  
+  $form->textarea('lst', 'LISTA', $lst);
+  
+
+  //echo "<br>".implode("<br>", $aCMDs)."<br><br>®";
+
+
 ?>
       
     </div>
